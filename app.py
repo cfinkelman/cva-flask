18657e1e0bf158a2cffb6b85ffac7f07c57f50f5
Chaim Finkelman <cfinkelman@rchsd.org>
2022-05-25 18:27:54 -0700

    report validation and start cnv

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/report/<report_type>")
-def report(report_type = None):
+def report(report_type = ''):
     return render_template(
         "report.html",
-        report_type=report_type
+        report_type=report_type.upper()
     )
 
--- static/index.js
+++ static/index.js
@@ -1,50 +1,92 @@
 // constants
 const controleIdsAndInfo = {
   // main page
-  patient_name: {},
-  sex: {},
-  date_of_birth: {},
-  indication_for_testing: {},
-  test_type: {},
-  ordering_provider: {},
-  hospital: {},
-  specimen: {},
-  collected: {},
-  received: {},
-  rcigm_case_id: {},
-  index_family_member_id: {},
-  report_date:{},
-  test_results: {},
-  confirmation_status: {},
-  gene_transcript: {},
-  condition: {},
-  genomic_coordinates: {},
-  variant: {},
-  zygosity: {},
-  sequence_variant_notes: {},
-  interpretation: {},
-  methodology_limitations_regulatory_disclosures: {},
-  approved_by: {},
-  // footer
-  patient: { key: 'patient_name' },
-  case_id: { key: 'rcigm_case_id' },
-  dob: { key: 'date_of_birth' },
+  patient_name: { prepopValue: 'patient_name' },
+  sex: { prepopValue: 'notspecified' },
+  date_of_birth: { prepopValue: '2019-07-27' },
+  indication_for_testing: { prepopValue: 'indication_for_testing' },
+  test_type: { prepopValue: 'test_type' },
+  ordering_provider: { prepopValue: 'ordering_provider' },
+  hospital: { prepopValue: 'hospital' },
+  specimen: { prepopValue: 'blood' },
+  collected: { prepopValue: 'collected' },
+  received: { prepopValue: 'received' },
+  case_id: { prepopValue: 'rcigm_case_id', validation: (value) => { return value.length === 6; } },
+  index_family_member_id: { prepopValue: 'index_family_member_id' },
+  report_date: { prepopValue: new Date().toISOString().substring(0,10) } ,
+  test_results: { prepopValue: 'test_results' },
+  confirmation_status: { prepopValue: 'confirmation_status' },
+  gene_transcript: { prepopValue: 'gene_transcript' },
+  condition: { prepopValue: 'condition' },
+  genomic_coordinates: { prepopValue: 'genomic_coordinates' },
+  variant: { prepopValue: 'variant' },
+  zygosity: { prepopValue: 'zygosity' },
+  sequence_variant_notes: { prepopValue: 'sequence_variant_notes' },
+  interpretation: { prepopValue: 'interpretation' },
+  methodology_limitations_regulatory_disclosures: { prepopValue: 'methodology_limitations_regulatory_disclosures' },
+  approved_by: { prepopValue: 'approved_by' },
+  approved_by_other: { key: 'approved_by' },
 }
 
 // load document
 $(document).ready(function() {
-  const [mainForm] = ['main_form'].map(v => document.getElementById(v));
+  const [mainForm, testResults] = ['main_form', 'test_results'].map(v => document.getElementById(v));
+  populateData(controleIdsAndInfo);
 
-  mainForm.addEventListener('change', handleChange)
-
-
+  document.addEventListener('keyup', syncFields);
+  mainForm.addEventListener('change', handleChange);
 });
 
 // helper function
+function populateData(data){
+  Object.keys(data).forEach(v => {
+    const value = data[v].prepopValue;
+    if(value !== undefined){
+      const el = document.getElementById(v);
+      el.value = value;
+      if(el.dataset.id){ $(`input[data-id="${el.dataset.id}"]`).val(value); }
+    }
+  });
+}
+
 function handleChange(e){
   const { id = '', value } = e.target;
-  const updateField = controleIdsAndInfo[id]?.key || id;
-  controleIdsAndInfo[updateField].value = value;
+  const updateField = e.target.dataset.id || id;
+  const validationFunction = controleIdsAndInfo[updateField]?.validation || (() => true);
+  const valid = validationFunction(value);
+  if(valid){
+    controleIdsAndInfo[updateField].value = value;
+  }
+  else{
+
+  }
+  document.getElementById(`cnt_div_${updateField}`).classList.toggle("has-error", !valid);
 
   console.log('change', updateField, controleIdsAndInfo);
+}
+
+function syncFields(e){
+  const syncField = e.target.dataset.id;
+  if(syncField){
+    $(`input[data-id="${syncField}"]`).val(e.target.value);
+  }
+  // add limit list
+  console.log('syncFields');
+}
+
+function handleElementChange(e){
+  const el = e.target;
+  if(isOverflowing(el)){
+    el.classList.add('overflowing');
+  }
+  else{
+    el.classList.remove('overflowing');
+  }
+
+  console.log('handleElementChange', el);
+}
+
+// https://www.geeksforgeeks.org/how-to-determine-the-content-of-html-elements-overflow-or-not/
+function isOverflowing(el){
+  return el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
 }
